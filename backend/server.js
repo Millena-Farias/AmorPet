bca2f7c359fca956fd55805704ac3fbbaa6ced53
mariamillenafarias10-ops <mariamillenafarias10@gmail.com>
2026-07-07 16:15:17 -0300

    corrigido ordem do sync para deploy

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,41 @@
-import express from  'express';
+import express from 'express';
 import sequelize from './models/bd.js'
 import clienteRoutes from './routes/clienteRoutes.js'
 import petRoutes from './routes/petRoutes.js'
 import agendamentoRoutes from './routes/agendamentoRoutes.js'
 import authRoutes from './routes/authRoutes.js'
-import   './models/relacionamentos.js'
+import Usuario from './models/Usuario.js'
+import Cliente from './models/Cliente.js'
+import Pet from './models/Pet.js'
+import Agendamento from './models/Agendamento.js'
+import './models/relacionamentos.js'
 import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
+
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use('/auth', authRoutes)
-
 app.use('/clientes', clienteRoutes)
-
 app.use('/pets', petRoutes)
 app.use('/agendamentos', agendamentoRoutes)
 
-
 async function testarConexao(){
-try{
-    await sequelize.authenticate();
-    console.log("Conexão estabelicida com sucesso")
-    await sequelize.sync({ force: false })
-    app.listen(3001, ()=>{
-        console.log("servidor rodando na porta 3001")
-    })
-}catch(error){
-    console.error("não foi possivel estabelecer uma conexão")
-    console.log(error)
-}
+    try{
+        await sequelize.authenticate();
+        console.log("Conexão estabelecida com sucesso")
+        await Usuario.sync({ alter: true })
+        await Cliente.sync({ alter: true })
+        await Pet.sync({ alter: true })
+        await Agendamento.sync({ alter: true })
+        app.listen(process.env.PORT || 3001, ()=>{
+            console.log("servidor rodando")
+        })
+    }catch(error){
+        console.error("não foi possivel estabelecer uma conexão")
+        console.log(error)
+    }
 }
 testarConexao()
 
